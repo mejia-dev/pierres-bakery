f22658d2a7023a921385b65639f0b78719b58ec4
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-19 10:38:17 -0800

    Pass third and fourth tests for Pastry

--- PierresBakery/Models/Pastry.cs
+++ PierresBakery/Models/Pastry.cs
@@ -5,10 +5,21 @@ namespace PierresBakery.Models
   public class Pastry
   {
     public int PerUnitCost { get; }
+    private static List<Pastry> _instances = new List<Pastry> { };
+
     public Pastry()
     {
       PerUnitCost = 2;
     }
 
+    public static List<Pastry> GetPastryBasket()
+    {
+      return _instances;
+    }
+
+    public static void DeleteBasket()
+    {
+      _instances.Clear();
+    }
   }
 }
