106d1cba8dcbd965f324307abfb019a50245dfb2
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-19 12:30:45 -0800

    Before lunch commit. Everything works except checkout / totals.

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -8,55 +8,83 @@ namespace PierresBakery
     static void Main()
     {
       Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("@-------------------------@");
       PickItem(0, 0);
     }
 
     static void PickItem(int pastriesCount = 0, int breadCount = 0)
     {
-      Console.Write("Would you like to buy pastries or bread?");
+      Console.WriteLine("Would you like to buy pastries or bread?");
+      Console.WriteLine("")
       string input = Console.ReadLine();
 
       if (input.ToLower().Contains("pastr"))
       {
-        // BuyPastry(pastriesCount, breadCount);
+        Console.WriteLine("");
+        Console.WriteLine("Each pastry costs $2. Buy 3 pastries, get the 4th one free!");
+        Console.WriteLine("You currently have {0} pastries in your cart.", breadCount);
+        BuyItem(pastriesCount, breadCount, "pastries");
       }
       else if (input.ToLower().Contains("bread") || input.ToLower().Contains("loa"))
       {
+        Console.WriteLine("");
         Console.WriteLine("Each loaf of bread costs $5. Buy 2 loaves, get the 3rd one free!");
-        Console.WriteLine("You currently have {0} loaves in your cart",breadCount);
-        BuyBread(pastriesCount, breadCount);
-        
+        Console.WriteLine("You currently have {0} loaves in your cart.", breadCount);
+        BuyItem(pastriesCount, breadCount, "loaves");
+
       }
       else
       {
+        Console.WriteLine("");
         Console.WriteLine("We unfortunately don't sell {0}.", input);
         PickItem(pastriesCount, breadCount);
       }
     }
 
-    static void BuyBread(int pastriesCount, int breadCount)
+    static void BuyItem(int pastriesCount, int breadCount, string wordForMultiple)
     {
-      
-      Console.Write("Enter the new total number of loaves you want to buy, or enter any other character to return to the previous screen: ")
+      Console.WriteLine("Enter the new total number of {0} you want to buy, or enter any other character to return to the previous screen: ", wordForMultiple);
       if (int.TryParse(Console.ReadLine(), out int inputtedAmount))
       {
-        Console.WriteLine("You now have {0} loaves in your cart. Would you like to checkout or keep shopping?", inputtedAmount);
+        Console.WriteLine("You now have {0} {1} in your cart. Would you like to checkout or keep shopping?", inputtedAmount, wordForMultiple);
         Console.WriteLine("(enter 'checkout' to checkout or any other key to keep shopping)");
         string navInput = Console.ReadLine();
-        if (navInput.ToLower.Contains("check"))
+        if (navInput.ToLower().Contains("check"))
         {
-
+          if (wordForMultiple == "loaves")
+          {
+            CheckoutMenu(pastriesCount, inputtedAmount);
+          }
+          else
+          {
+            CheckoutMenu(inputtedAmount, breadCount);
+          }
         }
         else
         {
-          PickItem(pastriesCount, inputtedAmount);  
+          if (wordForMultiple == "loaves")
+          {
+            Console.WriteLine("");
+            PickItem(pastriesCount, inputtedAmount);
+          }
+          else
+          {
+            Console.WriteLine("");
+            PickItem(inputtedAmount, breadCount);
+          }
         }
 
       }
       else
       {
+        Console.WriteLine("");
         PickItem(pastriesCount, breadCount);
       }
+    }
+
+    static void CheckoutMenu(int pastriesCount, int breadCount)
+    {
+
     }
 
     // static void BuyPastry(int pastriesCount, int breadCount)
@@ -69,21 +97,21 @@ namespace PierresBakery
 }
 
 
-// Console.Write("Enter the number of loaves you want to buy (enter 0 to exit): ");
-        if (int.TryParse(Console.ReadLine(), out int loaves))
-        {
-          if (loaves == 0)
-          {
-            Console.WriteLine("Thank you for shopping at Pierre's Bakery. Goodbye!");
-            break;
-          }
+// Console.WriteLine("Enter the number of loaves you want to buy (enter 0 to exit): ");
+// if (int.TryParse(Console.ReadLine(), out int loaves))
+// {
+//   if (loaves == 0)
+//   {
+//     Console.WriteLine("Thank you for shopping at Pierre's Bakery. Goodbye!");
+//     break;
+//   }
 
-          int totalCost = Bread.GetBasketTotal(loaves);
+//   int totalCost = Bread.GetBasketTotal(loaves);
 
-          Console.WriteLine($"Total cost for {loaves} loaves: ${totalCost}");
-          Console.WriteLine();
-        }
-        else
-        {
-          
-        }
+//   Console.WriteLine($"Total cost for {loaves} loaves: ${totalCost}");
+//   Console.WriteLine();
+// }
+// else
+// {
+
+// }
