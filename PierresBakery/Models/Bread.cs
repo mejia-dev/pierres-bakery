c381834cf362bca99707a4c49468fb380543cc77
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-19 09:32:55 -0800

    Pass third and fourth test for Bread

--- PierresBakery/Models/Bread.cs
+++ PierresBakery/Models/Bread.cs
@@ -1,11 +1,27 @@
+using System.Collections.Generic;
+
 namespace PierresBakery.Models
 {
   public class Bread
   {
     public int PerUnitCost { get; }
+    private static List<Bread> _instances = new List<Bread> {};
+
     public Bread()
     {
       PerUnitCost = 5;
+      _instances.Add(this);
     }
+
+    public static List<Bread> GetBreadBasket()
+    {
+      return _instances;
+    }
+
+    public static void DeleteBasket()
+    {
+      _instances.Clear();
+    }
+    
   }
 }
