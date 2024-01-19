133f77b1b9de6511d06bfd06dbaa4b296197a182
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-19 12:06:35 -0800

    WIP commit for UI

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -5,14 +5,71 @@ namespace PierresBakery
 {
   class Program
   {
-    static void Main(string[] args)
+    static void Main()
     {
       Console.WriteLine("Welcome to Pierre's Bakery!");
-      Console.WriteLine("Each loaf of bread costs $5. Buy 2 loaves, get the 3rd one free!");
+      PickItem(0, 0);
+    }
 
-      while (true)
+    static void PickItem(int pastriesCount = 0, int breadCount = 0)
+    {
+      Console.Write("Would you like to buy pastries or bread?");
+      string input = Console.ReadLine();
+
+      if (input.ToLower().Contains("pastr"))
       {
-        Console.Write("Enter the number of loaves you want to buy (enter 0 to exit): ");
+        // BuyPastry(pastriesCount, breadCount);
+      }
+      else if (input.ToLower().Contains("bread") || input.ToLower().Contains("loa"))
+      {
+        Console.WriteLine("Each loaf of bread costs $5. Buy 2 loaves, get the 3rd one free!");
+        Console.WriteLine("You currently have {0} loaves in your cart",breadCount);
+        BuyBread(pastriesCount, breadCount);
+        
+      }
+      else
+      {
+        Console.WriteLine("We unfortunately don't sell {0}.", input);
+        PickItem(pastriesCount, breadCount);
+      }
+    }
+
+    static void BuyBread(int pastriesCount, int breadCount)
+    {
+      
+      Console.Write("Enter the new total number of loaves you want to buy, or enter any other character to return to the previous screen: ")
+      if (int.TryParse(Console.ReadLine(), out int inputtedAmount))
+      {
+        Console.WriteLine("You now have {0} loaves in your cart. Would you like to checkout or keep shopping?", inputtedAmount);
+        Console.WriteLine("(enter 'checkout' to checkout or any other key to keep shopping)");
+        string navInput = Console.ReadLine();
+        if (navInput.ToLower.Contains("check"))
+        {
+
+        }
+        else
+        {
+          PickItem(pastriesCount, inputtedAmount);  
+        }
+
+      }
+      else
+      {
+        PickItem(pastriesCount, breadCount);
+      }
+    }
+
+    // static void BuyPastry(int pastriesCount, int breadCount)
+    // {
+    //   Console.WriteLine("Each pastry costs $2. Buy 3 pastries, get the 4th one free!");
+
+    // }
+
+  }
+}
+
+
+// Console.Write("Enter the number of loaves you want to buy (enter 0 to exit): ");
         if (int.TryParse(Console.ReadLine(), out int loaves))
         {
           if (loaves == 0)
@@ -28,9 +85,5 @@ namespace PierresBakery
         }
         else
         {
-          Console.WriteLine("Invalid input. Please enter a valid number.");
+          
         }
-      }
-    }
-  }
-}
