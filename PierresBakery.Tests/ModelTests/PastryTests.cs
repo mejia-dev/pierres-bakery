04090dd6d1fe52192a35bd2b018103344f9e0f87
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-19 10:37:29 -0800

    Write third and fourth tests for Pastry

--- PierresBakery.Tests/ModelTests/PastryTests.cs
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -6,8 +6,12 @@ using PierresBakery.Models;
 namespace PierresBakery.Tests
 {
   [TestClass]
-  public class PastryTests
+  public class PastryTests : IDisposable
   {
+    public void Dispose()
+    {
+      Pastry.DeleteBasket();
+    }
     [TestMethod]
     public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
     {
@@ -20,6 +24,24 @@ namespace PierresBakery.Tests
       Pastry newPastry = new Pastry();
       int cost = newPastry.PerUnitCost;
       Assert.AreEqual(cost, 2);
+    }
+    [TestMethod]
+    public void GetPastryBasket_ReturnsAllInstancesOfPastry_List()
+    {
+      Pastry Pastry1 = new Pastry();
+      Pastry Pastry2 = new Pastry();
+      Pastry Pastry3 = new Pastry();
+      List<Pastry> expectedPastryBasket = new List<Pastry> { Pastry1, Pastry2, Pastry3 };
+      List<Pastry> resultPastryBasket = Pastry.GetPastryBasket();
+      CollectionAssert.AreEqual(expectedPastryBasket, resultPastryBasket);
+    }
+    [TestMethod]
+    public void DeleteBasket_DeletesAllInstancesOfPastry_Void()
+    {
+      Pastry myPastry = new Pastry();
+      List<Pastry> expectedPastryBasket = new List<Pastry> { };
+      Pastry.DeleteBasket();
+      CollectionAssert.AreEqual(expectedPastryBasket, Pastry.GetPastryBasket());
     }
   }
 }
