743ba7ff81dd877fdffbb529941f7ff769446c66
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-19 11:14:55 -0800

    WIP commit for UI

--- PierresBakery/Program.cs
+++ PierresBakery/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using PierresBakery.Models;
 
-class Program
+namespace PierresBakery
 {
-  static void Main()
+  class Program
   {
-    
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Each loaf of bread costs $5. Buy 2 loaves, get the 3rd one free!");
+
+      while (true)
+      {
+        Console.Write("Enter the number of loaves you want to buy (enter 0 to exit): ");
+        if (int.TryParse(Console.ReadLine(), out int loaves))
+        {
+          if (loaves == 0)
+          {
+            Console.WriteLine("Thank you for shopping at Pierre's Bakery. Goodbye!");
+            break;
+          }
+
+          int totalCost = Bread.GetBasketTotal(loaves);
+
+          Console.WriteLine($"Total cost for {loaves} loaves: ${totalCost}");
+          Console.WriteLine();
+        }
+        else
+        {
+          Console.WriteLine("Invalid input. Please enter a valid number.");
+        }
+      }
+    }
   }
 }
