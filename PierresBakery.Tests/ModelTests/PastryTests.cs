2856dbe4bc383f233a99de57c11835f27cfcb12e
mejia-dev <aaron.mejia8484@gmail.com>
2024-01-19 10:18:25 -0800

    Write first test for Pastry

---
+++ PierresBakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using PierresBakery.Models;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class PastryTests : IDisposable
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry();
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+  }
+}
